0bb35f12a53bd9d0214c1e648339bc768f054447
Mario <mdiaz@sunkistgrowers.com>
2024-06-24 13:43:03 -0700

    Completeed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...toBeRemoved) {
+    return inputArray.filter((element) => !toBeRemoved.includes(element));
 };
 
 // Do not edit below this line
