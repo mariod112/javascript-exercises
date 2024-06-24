db46b8dd53c4a6c4665a65f47387194f28b2da8e
Mario <mdiaz@sunkistgrowers.com>
2024-06-24 11:53:00 -0700

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(inputString, timesToRepeat) {
 
+    if(timesToRepeat >= 0)
+    {
+        let outputString = "";
+
+        for(let count = 0; count < timesToRepeat; count++)
+        { 
+            outputString += inputString;
+        }
+
+        return outputString;
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
