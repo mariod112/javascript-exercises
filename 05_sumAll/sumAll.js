7d87c3864aca6a7f6e45902e9fe4107b6477c95f
Mario <mdiaz@sunkistgrowers.com>
2024-06-24 15:27:28 -0700

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(typeof(start) != 'number' || typeof(end) != 'number')
+    {
+        return 'ERROR';
+    }
+
+    if( start < 0 || end < 0)
+    {
+        return 'ERROR';
+    }
+
+    if (start > end)
+    {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let total = 0;
+
+    for (let i = start; i <= end; i++)
+    {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
