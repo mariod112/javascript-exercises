3e22ec26fec1efc98f7f567e1bdca29ae150108c
Mario <mdiaz@sunkistgrowers.com>
2024-06-25 14:05:23 -0700

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const punctuation = /[!.,\s]/g;
+    let cleanedInput = input.replace(punctuation, "").toLowerCase();
+    let reverse = cleanedInput;
 
+    reverse = reverse.split("").reverse().join("");
+    return cleanedInput === reverse;
 };
 
 // Do not edit below this line
