2d0048823c6bdbe2f96f4d55ea4db9d41e21b660
Mario <mdiaz@sunkistgrowers.com>
2024-06-25 15:26:56 -0700

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) =>{
 
+        let personDeath = person.yearOfDeath;
+        let oldestPersonDeath = oldestPerson.yearOfDeath;
+
+        if(personDeath === undefined)
+        {
+            personDeath = new Date().getFullYear();
+        }
+
+        if(oldestPersonDeath === undefined)
+        {
+            oldestPersonDeath = new Date().getFullYear();
+        }
+
+        let age = personDeath - person.yearOfBirth;
+        let oldestAge = oldestPersonDeath - oldestPerson.yearOfBirth;
+
+        if (age > oldestAge)
+        {
+            return person;
+        }
+        else
+        {
+            return oldestPerson;
+        }
+
+    });
 };
 
 // Do not edit below this line
